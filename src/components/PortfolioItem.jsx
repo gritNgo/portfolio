d7dfdb4ec35ef7c64d1287b8d2e39465496821aa
Fiorenso Fernando <dustspeck00@gmail.com>
2023-12-28 21:04:23 +0000

    updated thumbnail sizes

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -4,9 +4,9 @@ function PortfolioItem({title, imgUrl, stack, link}) {
   return (
     <div className='border-2 border-stone-900 rounded-md overflow-hidden'>
         <img 
-        src="{imgUrl}" 
+        src={imgUrl}
         alt="project" 
-        className='w-full h-36 md:h48 object-cover cursor pointer'
+        className='w-full h-36 md:h-48 object-cover cursor pointer'
         />
         <div className='w-full p-4'>
             <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>
